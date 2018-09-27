2e45cac46bb08c88be5cad7f716cc0d167869280
Amo Moloko <u14284783@tuks.co.za>
2018-09-27 16:40:56 +0200

    Query functionality working

--- src/components/Driver.js
+++ src/components/Driver.js
@@ -6,6 +6,11 @@ class Driver extends Component {
             <div>
                 <div>
                     {this.props.driver.name} ({this.props.driver.team})
+                    {this.props.driver.points} 
+                    <img src={this.props.driver.pictureURL} />
+                    {this.props.driver.country}
+                    {this.props.driver.podiums}
+                    {this.props.driver.championshipWins} 
                 </div>
             </div>
         )
--- src/components/DriverList.js
+++ src/components/DriverList.js
@@ -1,24 +1,51 @@
 import React, {Component} from 'react';
 import Driver from './Driver';
+import {Query} from 'react-apollo';
+import gql from 'graphql-tag';
+
+
+const FEED_QUERY = gql`
+  {
+      feed{
+          drivers{
+              id
+              name
+              team
+              name
+    team
+    points
+    pictureURL
+    country
+    podiums
+    championshipWins
+          }
+      }
+  }
+`
 
 class DriverList extends Component {
     render(){
-        const driversToRender = [
-            {
-                id: '1',
-                name: 'Lonely',
-                team: 'Apple'
-            },
-            {
-                id: '2',
-                name: 'Happy',
-                team: 'Genius'
-            }
-        ]
+        
+   return(
+    <Query query={FEED_QUERY}>
+    {({loading, error, data}) => {
+        if (loading) return <div>Fetching</div>
+        if (error) return <div>Error</div>
+
+        const driversToRender = data.feed.drivers
 
         return(
-        <div>{driversToRender.map(driver => <Driver key={driver.id} driver={driver}/>)}</div>
-        )
+            <div>
+
+                            { driversToRender.map(driver => <Driver key={driver.id} driver={driver}/>)}
+
+            </div>
+      )
+    }}
+            </Query>
+
+   )
+        
     }
 }
 
