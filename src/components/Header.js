4576ed277208776883b1cad3be31a391204dc16d
Amo Moloko <u14284783@tuks.co.za>
2018-10-11 15:37:02 +0200

    trying implement a responsive navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,33 +5,59 @@ import {AUTH_TOKEN} from '../constants'
 import '../styles/Header.css';
 
 class Header extends Component{
+
+     toggleNav = ({ target })  => {
+        const navButton = document.querySelector('button[aria-expanded]');
+        const expanded = target.getAttribute('aria-expanded') === 'true' || false;
+        navButton.setAttribute('aria-expanded', !expanded);
+      }
+
    render(){
     const authToken = localStorage.getItem(AUTH_TOKEN)
 
        return (
-           <nav className="flex bb justify-between  red">
-           <div className="flex flex-fixed white">
-           <div className=" ml4 mr5"><h1>Formula One</h1></div>
+        <nav className="menu bb  red">
+        <button aria-expanded="false"  onClick={this.toggleNav} aria-controls="menu-list">
+          <span className="open">☰</span>
+          <span className="close">×</span>
+          Menu
+        </button>
+        <div className="flex flex-fixed white">
 
-
-        
-         <Link to="/" className="ml5 mt2 grow dtc no-underline black">
+        <div className="fw7 mr1 white"><h1>Forumla One</h1></div>
+        <ul id="menu-list">
+          <li>
+          <Link to="/" 
+          className="ml5  grow dtc no-underline black">
            <h3>New</h3>
            </Link>
-       
-       
-           <Link to="/top" className="ml3 grow mt2 no-underline black">
+          </li>
+
+          <li>
+          <Link to="/top" className="ml3 grow dtc no-underline black">
            <h3>Top</h3>
-           </Link>
-           <Link to="/search" className="mt2 grow  ml3 dtc no-underline black">
+           </Link>          
+           </li>
+
+          <li>
+          <Link to="/search" className="mt2 grow  dtc no-underline black">
            <h3>Search</h3>
            </Link>
-           <Link to="/Circuits" className="mt2 grow ml3 dtc no-underline black">
+          </li>
+
+          <li>
+          <Link to="/Circuits" className="mt2 grow  dtc no-underline black">
            <h3>Circuits</h3>
-           </Link>
-           <Link to="/theatre" className="mt2 grow ml3 dtc no-underline black">
+           </Link>          
+           </li>
+
+          <li>
+          <Link to="/theatre" className="mt2 grow  dtc no-underline black">
            <h3>Theatre</h3>
-           </Link>
+           </Link>          
+           </li>
+
+           <li>
            {authToken &&(
                <div className="flex">
                <Link to="/create" className="ml3 grow mt2 no-underline white">
@@ -39,17 +65,9 @@ class Header extends Component{
                </Link>
                </div>
            )}
-         </div>
-         
+           </li>
 
-    
-          
-
-           
-          
-        
-           <div className="flex flex-fixed">
-             {authToken ? (
+           {authToken ? (
                  <div 
                  className="ml1 mt2 grow pointer red"
                  onClick={() => {
@@ -59,12 +77,21 @@ class Header extends Component{
                  <h3>Logout</h3>
                  </div>
              ): (
+                 <li>
                 <Link to="/login" className="mr4 grow mt2 no-underline white">
                <h3>Login</h3>
                </Link>
+               </li>
              )}
+        </ul>
+        
+          </div>
+
+
+         <div className="flex flex-fixed">
+            
            </div>
-           </nav>
+      </nav>
        )
    }
 }
