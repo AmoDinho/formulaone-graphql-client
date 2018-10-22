11769c92d95810742d57d07678a55b9a5855ecda
Amo Moloko <u14284783@tuks.co.za>
2018-10-22 15:39:19 +0200

    Allows user to view individual driver. It just needs styling

---
+++ src/containers/Driver.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo';
+
+const DRIVER_QUERY = gql`
+ query DRIVER_QUERY($id:ID!){
+     driver(id: $id){
+         id
+         name
+         team
+         points
+         pictureURL
+         podiums
+         championshipWins
+         country
+     }
+ }
+`
+
+class Driver extends Component {
+    render(){
+        const id = this.props.match.params.id;
+        return (
+            <Query
+            query={DRIVER_QUERY}
+            variables={{
+                id
+            }}>
+            { ({error,loading,data}) =>{
+                if (loading) return <div>Fetching</div>
+                if (error) return <div>Error</div>;
+
+                const driver = data.driver;
+                return(
+                    <div>
+
+
+                          {driver.name}
+                    {driver.team}
+                    {driver.points}
+                    <img src={driver.pictureURL} alt="driver picture"/>
+                    {driver.podiums}
+                    {driver.championshipWins}
+                    {driver.country}
+                    </div>
+                  
+                );
+            }}
+            </Query>
+        )
+    }
+}
+
+export default Driver;
