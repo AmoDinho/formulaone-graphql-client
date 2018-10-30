4bfa294abbd46dd1339c78c342007a22c5cb21cc
Amo Moloko <u14284783@tuks.co.za>
2018-10-30 12:44:34 +0200

    Last commit to make it work

---
+++ src/components/Modal.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import * as Icon from 'react-feather';
+
+ const Modal = ({handleClose,show,children}) => {
+    const showHideClassName = show ? 'modal display-block':'model display-none';
+
+    return(
+        <div className={showHideClassName}>
+        <section className='modal-main'>
+            <Icon.X className="modal_button_close pointer" onClick={handleClose}/>
+        
+        {children}
+        
+        
+        </section>
+        </div>
+    )
+}
+
+export default Modal;
