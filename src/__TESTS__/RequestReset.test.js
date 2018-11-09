db27d86c35240ac010e93dd6eb34a19e9f892f29
amodinho <u14284783@tuks.co.za>
2018-11-09 13:21:08 +0200

    Working on Request Reset Test

--- src/__TESTS__/RequestReset.test.js
+++ src/__TESTS__/RequestReset.test.js
@@ -3,18 +3,86 @@ import {MockedProvider} from "react-apollo/test-utils";
 import renderer from "react-test-renderer";
 import {MemoryRouter } from "react-router-dom";
 const wait = require('waait');
+import {mount,configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {wrap} from 'module';
+import RequestReset, {REQUEST_RESET_MUTATION} from '../containers/RequestReset';
 
+
+//configure enzyme adpater
+configure({adapter: new Adapter()});
+
+const mocks = [
+    {
+        request: {
+            query: REQUEST_RESET_MUTATION,
+            variables: {email: 'hans@interstellar.io'}
+        },
+        result: {
+            data: 
+            {
+            RequestReset: {message:'success', _typename: 'Message'},
+        }
+      }
+    }
+];
+
+const mocksError = [
+    {
+        request: {
+            query: REQUEST_RESET_MUTATION,
+            variables: {email: 'baz@interstellar.io'}
+        },
+        result: {
+            data: 
+            {
+            RequestReset:{message:'failure',_typename:'Message'}
+        }
+      }
+    }
+];
 
 it('loads the page',() =>{
-
+  renderer.create(
+      <MockedProvider mocks={[]}>
+        <RequestReset />
+      </MockedProvider>
+  )
 });
 
 
-it('successfully sends a reset',() =>{
+it('successfully sends a reset', async () =>{
+  const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+       <RequestReset/>
+      </MockedProvider>
+  );
 
+  wrapper
+  .find('input')
+  .simulate('change',{target: {name:'email',value:'hans@interstellar.io'}});
+          
+  wrapper.find('PrimaryButton').simulate('click');
+  await wait();
+  wrapper.update();
+  expect(wrapper.find('p').text()).toContain('Check your email for a link!');        
 });
 
-it('it does not send email if the email does not exisit',() =>{
+it('it does not send email if the email does not exist', async () =>{
+    const wrapper = mount(
+        <MockedProvider mocks={mocksError}>
+         <RequestReset/>
+        </MockedProvider>
+    );
+ 
+    wrapper
+  .find('input')
+  .simulate('change',{target: {name:'email',value:'baz@interstellar.io'}});
+
+  wrapper.find('PrimaryButton').simulate('click');
+  await wait();
+  wrapper.update();
+  expect(wrapper.find('p').text()).toContain('That email does not exist on our side.');
 
 });
 
--- src/containers/RequestReset.js
+++ src/containers/RequestReset.js
@@ -1,12 +1,61 @@
 import React from 'react'
+import gql from 'graphql-tag';
+import { Mutation} from 'react-apollo';
+import PrimaryButton from "../components/PrimaryButton"
+export const REQUEST_RESET_MUTATION = gql`
+ mutation REQUEST_RESET_MUTATION($email:String!){
+     requestReset(email:$email){
+        SuccessMessage
+     }
+ }
+`
 
 class RequestReset extends React.Component{
-  
+  state = {
+      email: ''
+  };
+
+  saveToState = e => {
+      this.setState({[e.target.name]: e.target.value});
+  };
 
     render(){
         return(
             <div>
             <h1>Request Password Reset</h1>
+            <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
+              {(reset, {error,loading,called}) => (
+               <form method="post"
+               >
+               {!error && !loading && called && <p>Check your email for a link!</p>}
+               {error && !loading && !called && <p>That email does not exist on our side.</p>}
+               <label htmlFor="email">
+               Email
+               <input
+               type="email"
+               placeholder="email"
+               value={this.state.email}
+               onChange={this.saveToState}
+               />
+               </label>
+             
+
+              <PrimaryButton
+            text="Send Email"
+            onClick={async e => {
+                e.preventDefault();
+                await reset();
+                this.setState({email: ''});
+            }}
+            />
+               </form>
+
+              )}
+
+           
+
+            </Mutation>
+
             </div>
           )
 
