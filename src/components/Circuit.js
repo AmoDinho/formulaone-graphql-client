cb42a643fe34a6e3ba5c40c1ac32a53f35b99d21
amodinho <u14284783@tuks.co.za>
2018-11-15 18:14:40 +0200

    Styling the circuit cards

--- src/components/Circuit.js
+++ src/components/Circuit.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import '../styles/Circuit.css';
+import img from '../1f1eb-1f1f7.png';
 
  const Circuit = ({circuit}) => {
-  
+    
         return(
-            <div>
-            <p>{circuit.name} - {circuit.country}</p>
+            <div className='circuit'>
+
+            <div className='circuit_hero'>
+            <img className='circuit_thumbnail' src={circuit.trackImage} alt='Circuits country'/>
+            </div>
+            <div className='circuit_details'>
+            <p className='circuit_details_name'>{circuit.name}</p>
+          
+          
+           <img src={img} className='circuit_details_emoji'  alt="ha"/>
+            </div>
+            
             </div>
         )
     
