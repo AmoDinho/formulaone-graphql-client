340b551956066107de8e0e4fc4a2f7bf20b087b2
amodinho <u14284783@tuks.co.za>
2018-11-16 15:31:14 +0200

    small updates

--- src/constants.js
+++ src/constants.js
@@ -5,12 +5,12 @@ export const LINKS_PER_PAGE = 5;
 export const emojis= [
     {
         country: 'China',
-        url: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/flag-for-russia_1f1f7-1f1fa.png'
+        url: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/flag-for-china_1f1e8-1f1f3.png'
     },
 
     {
         country: 'Russia',
-        url: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/flag-for-china_1f1e8-1f1f3.png'
+        url: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/flag-for-russia_1f1f7-1f1fa.png'
     },
     {
         country: 'AZERBAIJAN',
