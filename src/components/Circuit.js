2e109b937237fdd48777c3d5030649c08fe6d8a2
amodinho <u14284783@tuks.co.za>
2018-11-16 12:45:13 +0200

    Emoji flags render based on the country

--- src/components/Circuit.js
+++ src/components/Circuit.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react';
+import {emojis} from '../constants';
 import '../styles/Circuit.css';
 import img from '../1f1eb-1f1f7.png';
 
+
+function lookUpURL(country, prop){
+    console.log(country)
+    let nation = emojis.find((x) => x.country === country)
+    console.log(nation)
+    return nation ? nation.hasOwnProperty(prop) ? nation[prop] : 'no such prop' : 'no such country'
+    
+}
+
+
+
  const Circuit = ({circuit}) => {
     
-        return(
+     return (
+    
             <div className='circuit'>
 
             <div className='circuit_hero'>
             <img className='circuit_thumbnail' src={circuit.trackImage} alt='Circuits country'/>
             </div>
+            
             <div className='circuit_details'>
             <p className='circuit_details_name'>{circuit.name}</p>
           
-          
-           <img src={img} className='circuit_details_emoji'  alt="ha"/>
+           
+             <img src={lookUpURL(circuit.country, 'url')} className='circuit_details_emoji'  alt="ha"/>
+
             </div>
             
             </div>
         )
+
+    
     
 }
 
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,19 @@
 export const AUTH_TOKEN = 'auth-token';
 
 export const LINKS_PER_PAGE = 5;
+
+export const emojis= [
+    {
+        country: 'China',
+        url: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/flag-for-russia_1f1f7-1f1fa.png'
+    },
+
+    {
+        country: 'Russia',
+        url: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/flag-for-china_1f1e8-1f1f3.png'
+    },
+    {
+        country: 'AZERBAIJAN',
+        url: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/flag-for-azerbaijan_1f1e6-1f1ff.png'
+    }
+]
