57228b20126036acc91aad1c28a5a7c93015b83d
amodinho <u14284783@tuks.co.za>
2018-11-19 17:29:01 +0200

    Trying to add search to the circuit list.

--- src/containers/CircuitList.js
+++ src/containers/CircuitList.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 import gql from 'graphql-tag';
 import {LINKS_PER_PAGE} from '../constants';
 import Circuit from '../components/Circuit';
+import {withApollo} from 'react-apollo';
 import '../styles/CircuitList.css';
 
 
@@ -20,20 +21,43 @@ export const TRACK_QUERY =gql`
       }
   }
 `
+export const TRACK_SEARCH_QUERY = gql`
+ query TRACK_SEARCH_QUERY($filter:String!){
+     circuits{
+          id
+          name
+          country
+          trackImage
+     }
+ }
+`
 
 
 class CircuitList extends Component {
+
+    state={
+   filter: '',
+   circuitsToRender: []
+    }
     render(){
 
         return(
            
             <div>
+                <div>
+                    Search <input
+                    type="text"
+                    onChange={e => this.setState({filter: e.target.value})}
+                    />
+                    <button onClick={() => this._search()}>Ok</button>
+                </div>
                 <Query query={TRACK_QUERY}>
                 {({loading, error, data }) =>{
                  if (loading) return <div>Fetching...</div>
                  if (error) return <div>Something went wrong</div>
 
                  const circuitsToRender = data.tracks.circuits
+                 
                  return (
                     <div className="circuit_list" >
                   {circuitsToRender.map(circuit => (
@@ -53,6 +77,18 @@ class CircuitList extends Component {
             
         )
     }
+
+    _search = async () => {
+        const {filter} = this.state
+        const result = await this.props.client.query({
+            query: TRACK_SEARCH_QUERY,
+            variables: {filter}
+        })
+        const circuitsToRender = result.data.tracks.circuits
+        this.setState({circuitsToRender})
+    }
 }
 
-export default CircuitList
+
+
+export default withApollo(CircuitList)
