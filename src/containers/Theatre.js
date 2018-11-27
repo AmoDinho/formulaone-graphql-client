387698e677a209df82fa9caba11d5f58b4a99a27
amodinho <u14284783@tuks.co.za>
2018-11-27 14:43:17 +0200

    styling theatre page & enable fetching of other playlists

--- src/containers/Theatre.js
+++ src/containers/Theatre.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
+import YouTube from '@u-wave/react-youtube';
+import Slider from 'react-slick';
+import '../styles/Theatre.css';
 
-
-const PATH_BASE = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&'
-const PLAYLIST = 'playlistId=PLfoNZDHitwjXFH-NLpwL8eXzmxzJTUV2A&'
+const PATH_BASE = 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&';
+const PLAYLIST = 'playlistId=PLfoNZDHitwjXFH-NLpwL8eXzmxzJTUV2A&';
+const HIGHLIGHTS = 'playlistId=PLfoNZDHitwjUUrM4dYe542iCcRpEzS_RX';
+const GUIDES = 'playlistId=PL2vgf11SN4RhB3Lz-eaREjLNERKAqVtoZ'
 const KEY = `key=${process.env.REACT_APP_YOUTUBE_KEY}`
 
 class Theatre extends Component {
@@ -10,43 +14,89 @@ class Theatre extends Component {
      super(props);
 
      this.state={
-         playlists:[]
+         playlists:[],
+         highlights:[],
+         guides:[]
     }
 
     this.fetchPlaylists = this.fetchPlaylists.bind(this);
-
+    this.fetchHighlights = this.fetchHighlights.bind(this);
+    this.fetchGuides = this.fetchGuides.bind(this);
    
  }
  fetchPlaylists(playlists){
     fetch(`${PATH_BASE}${PLAYLIST}${KEY}`)
     .then(response => response.json())
-    //.then(result => this.setState(playlists))
-    .then(result => console.log(result))
+    .then(result => this.setState({playlists: result.items}))
+    //.then(result => console.log(result))
     .catch(error => error)
 
 }
 
+fetchGuides(guides){
+    fetch(`${PATH_BASE}${GUIDES}${KEY}`)
+    .then(response => response.json())
+    .then(result => this.setState({guides: result.items}))
+    //.then(result => console.log(result))
+    .catch(error => error)
+
+}
+
+fetchHighlights(highlights){
+    fetch(`${PATH_BASE}${HIGHLIGHTS}${KEY}`)
+    .then(response => response.json())
+    .then(result => this.setState({highlights: result.items}))
+    //.then(result => console.log(result))
+    .catch(error => error)
+
+}
 
 componentDidMount(){
 
-   this.fetchPlaylists()
+   this.fetchPlaylists();
+   this.fetchGuides();
+   this.fetchHighlights();
 }
 
     render(){
 
         const {playlists} = this.state;
+        console.log(playlists)
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerPadding: 50
+        };
         return (
             <div>
                 <h1>Theatre</h1>
-            <ul>
+
+
+                <div className="theatre__paddock-pass">
+                <Slider {...settings}>
                 {
-                    playlists.map(items =>
-                    <li key={items.id}>
-                    <p>{items.contentDetails.videoId}</p>
-                    </li>
-                    )
-                }
-            </ul>
+                playlists.map(items =>
+                    <div className="theatre__video" key={items.id}>
+                    <YouTube
+                    
+                    video={items.contentDetails.videoId}
+                    
+                    />
+                   </div>
+    
+    
+               )
+                     }
+               </Slider>
+                </div>
+                <div className="theatre__race-highlights">
+                </div>
+                <div className="theatre__circuit-guide">
+                </div>
+            
             </div>
         )
     }
